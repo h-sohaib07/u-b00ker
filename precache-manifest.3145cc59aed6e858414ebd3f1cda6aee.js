eb6694ab2b4e251ef8fed4c54b484704f2d08d84
SohaibHassan <sohaib.hassan@phenologix.com>
2020-01-29 22:06:54 +0500

    Updates

---
+++ precache-manifest.3145cc59aed6e858414ebd3f1cda6aee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51966e60d6a67c92b0b1ad91925c8979",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/index.html"
+  },
+  {
+    "revision": "057a5c1c1a5be55835a1",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c608f39ba77d5dbd30a6",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/css/main.51055f13.chunk.css"
+  },
+  {
+    "revision": "057a5c1c1a5be55835a1",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/js/2.88ee8e2e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/js/2.88ee8e2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c608f39ba77d5dbd30a6",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/js/main.da2c56ef.chunk.js"
+  },
+  {
+    "revision": "e9327780acdd8a9a6776",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/js/runtime-main.84c88515.js"
+  },
+  {
+    "revision": "1a6fb71b76c2685c9b1b6d0375a71761",
+    "url": "//h-sohaib07.github.io/u-b00ker/public/static/media/login-2.1a6fb71b.jpg"
+  }
+]);
